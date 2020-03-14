ee3e59c4a244f0c7edc8e32827db204a71cca65c
sourque <shane@sourque.dev>
2020-03-14 00:51:13 -0700

    Implemented structs, reading configuration, basic scoring, and docs

---
+++ install.sh
@@ -0,0 +1,19 @@
+############################################################
+cat << EOF
+
+ .oooo.    .ooooo.   .oooo.    .ooooo.  oooo  oooo   .oooo.o
+\`P  )88b  d88' \`88b \`P  )88b  d88' \`"Y8 \`888  \`888  d88(  "8
+ .oP"888  888ooo888  .oP"888  888        888   888  \`"Y88b.
+d8(  888  888    .o d8(  888  888   .o8  888   888  o.  )88b
+\`Y888""8o \`Y8bod8P' \`Y888""8o \`Y8bod8P'  \`V88V"V8P' 8""888P'
+
+EOF
+############################################################
+
+apt update
+apt install -y software-properties-common
+add-apt-repository ppa:longsleep/golang-backports
+apt update
+apt install -y golang-go git
+go get "github.com/urfave/cli"
+go get "github.com/BurntSushi/toml/cmd/tomlv"
