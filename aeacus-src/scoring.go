18d78b0ef30018e47960557a3dd9cc039dd8ad72
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2020-04-11 17:45:09 -0700

    Audio
    Changed playGain and playAlarm into playAudio and added windows support

--- aeacus-src/scoring.go
+++ aeacus-src/scoring.go
@@ -28,10 +28,10 @@ func scoreImage(mc *metaConfig, id *imageData) {
 		prevScore, _ := strconv.Atoi(prevPoints)
 		if prevScore < id.Score {
 			sendNotification(mc.Config.User, "You gained points!")
-			playGain("/opt/aeacus/web/assets/gain.wav")
+			playAudio(mc.DirPath + "web/assets/gain.wav")
 		} else if prevScore > id.Score {
 			sendNotification(mc.Config.User, "You lost points!")
-			playAlarm("/opt/aeacus/web/assets/alarm.wav")
+			playAudio(mc.DirPath + "web/assets/alarm.wav")
 		}
 	} else {
 		fmt.Println(err)
