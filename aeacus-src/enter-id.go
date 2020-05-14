01dd03a80983ba0e4501806532f6cfc45619831f
safinsingh <safin.singh@gmail.com>
2020-05-13 22:38:51 -0700

    Add GUI for ID

---
+++ aeacus-src/enter-id.go
@@ -0,0 +1,52 @@
+package main
+
+// I just copied my code from my testing folder, needs to be optimized to work with Aeacus
+
+import (
+	"fmt"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
+)
+
+var id string
+
+type enterEntry struct {
+	widget.Entry
+}
+
+func (e *enterEntry) onEnter() {
+	fmt.Println(e.Entry.Text)
+	e.Entry.SetText("")
+}
+
+func newEnterEntry() *enterEntry {
+	entry := &enterEntry{}
+	entry.ExtendBaseWidget(entry)
+	return entry
+}
+
+func main() {
+	a := app.New()
+	a.Settings().SetTheme(theme.LightTheme())
+	w := a.NewWindow("Enter ID")
+	w.CenterOnScreen()
+
+	entry := newEnterEntry()
+
+	w.SetContent(widget.NewVBox(
+		widget.NewLabelWithStyle("Enter ID:", fyne.TextAlignCenter, fyne.TextStyle{}),
+		entry,
+		widget.NewButton("Save", func() {
+			id = entry.Text
+			a.Quit()
+		}),
+	))
+
+	w.Resize(fyne.Size{Width: 400})
+	w.ShowAndRun()
+
+	fmt.Println(id)
+}
