edf2877c8b86a4cac41d0efe5ddfc172f68eb693
Aadit Gupta <50186752+aaditgupta21@users.noreply.github.com>
2020-07-21 21:08:08 -0700

    Add Linux GUI that writes to scoring.conf (#5)
    * Created Linux Gui that writes to scoring.conf
    * Update gui_linux.sh
    (Thanks to https://github.com/aaditgupta21 for authoring and https://github.com/safinsingh for review!)
    
    Integrate and format Linux configuration gui script

--- src/gui_linux.go
+++ src/gui_linux.go
@@ -3,3 +3,11 @@ package main
 func launchIDPrompt() {
 	// zenity prompt ree?
 }
+
+func launchConfigGui() {
+	warnPrint("The script doesn't currently have the ability to add multiple check or fail conditions-- you must still do these manually.")
+    _, err := shellCommandOutput("bash ./misc/gui_linux.sh")
+    if err == nil {
+    	infoPrint("Configuration successfully written to scoring.conf!")
+    }
+}
