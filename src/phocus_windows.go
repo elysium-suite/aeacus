06ae89d4c80e70d6bb8eab99a595c30a5940cfc8
Shane Donahue <shane@sourque.dev>
2020-07-27 16:08:43 -0700

    Add time delay to running phocus.exe

--- src/phocus_windows.go
+++ src/phocus_windows.go
@@ -49,6 +49,7 @@ func main() {
 func (p *program) Init(env svc.Environment) error {
 	if !env.IsWindowsService() && !*idgui {
 		failPrint("Sorry! Don't run this binary. It's probably already running as a Windows service (CSSClient).")
+		time.Sleep(5 * time.Second)
 		os.Exit(1)
 	}
 	return nil
