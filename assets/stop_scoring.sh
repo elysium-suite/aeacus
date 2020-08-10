22d4acb75679114f23333e6ec89c7c6376f3e4fa
sourque <shane@sourque.dev>
2020-08-10 03:21:51 -0700

    Remove concurrency for Windows, fix SecurityPolicy and UserExistsNot checks

--- assets/stop_scoring.sh
+++ assets/stop_scoring.sh
@@ -4,12 +4,11 @@ zenity --question \
         --text="Would you like to stop scoring for this image?" \
         --title="Aeacus SE"
 
-
 if [[ $? -eq 0 ]]; then
-    notify-send -i /opt/aeacus/web/assets/logo.png "Stopping scoring and shutting down"
-    service aeacus-client stop
-    rm -rf /opt/aeacus/phocus
-    rm -rf /opt/aeacus/scoring.dat
+    notify-send -i /opt/aeacus/assets/logo.png "Stopping scoring, and shutting down."
+    service CSSClient stop
+    pkill -9 phocus
+    rm -f /opt/aeacus/phocus /opt/aeacus/scoring.dat
     shutdown now
 else
     notify-send -i /opt/aeacus/assets/logo.png "Confirmation failed!"
