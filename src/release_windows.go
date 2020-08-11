c82492d85f7232ece15827c9411337157b5954e9
Altoid0 <39836458+Altoid0@users.noreply.github.com>
2020-08-10 21:48:33 -0700

    Revamp ID GUI (#51)
    * revamped ID gui added installfont func and assets
    
    * renamed asset files, changed in posh gui
    
    * Deleted unneeded font files and readme

--- src/release_windows.go
+++ src/release_windows.go
@@ -84,6 +84,40 @@ func configureAutologin() {
 	shellCommand(powershellAutoLogin)
 }
 
+func installFont() {
+	if verboseEnabled {
+		infoPrint("Installing Raleway font for Winform...")
+	}
+	powershellFontInstall := `
+	$SourceDir   = "C:\aeacus\assets\Raleway"
+	$Source      = "C:\aeacus\assets\Raleway\*"
+	$Destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+	$TempFolder  = "C:\Windows\Temp\Fonts"
+
+	# Create the source directory if it doesn't already exist
+	New-Item -ItemType Directory -Force -Path $SourceDir | Out-Null
+
+	New-Item $TempFolder -Type Directory -Force | Out-Null
+
+	Get-ChildItem -Path $Source -Include '*.ttf','*.ttc','*.otf' -Recurse | ForEach {
+		If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {
+
+			$Font = "$TempFolder\$($_.Name)"
+			
+			# Copy font to local temporary folder
+			Copy-Item $($_.FullName) -Destination $TempFolder
+			
+			# Install font
+			$Destination.CopyHere($Font,0x10)
+
+			# Delete temporary copy of font
+			Remove-Item $Font -Force
+		}
+	}
+	`
+	shellCommand(powershellFontInstall)
+}
+
 func installService() {
 	if verboseEnabled {
 		infoPrint("Installing service with sc.exe...")
