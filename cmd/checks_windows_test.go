87f54d5f4007126de7d4c3f87e2c9f25b5e4d074
Safin Singh <safin.singh@gmail.com>
2020-08-28 16:27:29 -0700

    more string escapes

--- cmd/checks_windows_test.go
+++ cmd/checks_windows_test.go
@@ -7,6 +7,6 @@ import (
 func TestRegistryKeyExists(t *testing.T) {
 	out, err := registryKey(`HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CrashControl`, "Overwrite", true)
 	if err != nil || out != true {
-		t.Error("registryKey(`HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CrashControl`, \"Overwrite\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
+		t.Error("registryKey(`HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CrashControl`\", \"Overwrite\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
 	}
 }
