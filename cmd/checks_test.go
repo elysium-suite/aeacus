850a8d802402601874b797b57a9c396283e6d0d7
Safin Singh <safin.singh@gmail.com>
2020-08-28 15:56:02 -0700

    add some error outputs

--- cmd/checks_test.go
+++ cmd/checks_test.go
@@ -12,28 +12,28 @@ func boolToString(b bool) string {
 func TestCommandOutput(t *testing.T) {
 	out, err := commandOutput(`echo 1`, "1")
 	if err != nil || out != true {
-		t.Error("commandOutput(`echo 1`, \"1\") got " + boolToString(out) + ", want `true`")
+		t.Error("commandOutput(`echo 1`, \"1\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
 	}
 }
 
 func TestCommandContains(t *testing.T) {
 	out, err := commandContains(`echo hello world`, "hello")
 	if err != nil || out != true {
-		t.Error("commandContains(`echo hello world!`, \"hello\") got " + boolToString(out) + ", want `true`")
+		t.Error("commandContains(`echo hello world!`, \"hello\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
 	}
 }
 
 func TestPathExists(t *testing.T) {
 	out, err := pathExists("/")
 	if err != nil || out != true {
-		t.Error("pathExists(\"/\") got " + boolToString(out) + ", want `true`")
+		t.Error("pathExists(\"/\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
 	}
 }
 
 func TestFileContains(t *testing.T) {
 	out, err := fileContains("../misc/tests/TestFileContains.txt", "world")
 	if err != nil || out != true {
-		t.Error("fileContains(\"../misc/tests/TestFileContains.txt\", \"hello\") got " + boolToString(out) + ", want `true`")
+		t.Error("fileContains(\"../misc/tests/TestFileContains.txt\", \"hello\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
 	}
 }
 
@@ -47,6 +47,6 @@ func TestFileContainsRegex(t *testing.T) {
 func TestDirContainsRegex(t *testing.T) {
 	out, err := dirContainsRegex("../misc/tests/dir", "^efgh")
 	if err != nil || out != true {
-		t.Error("dirContainsRegex(\"../misc/tests/dir\", \"^efgh\") got " + boolToString(out) + err.Error() + ", want `true`")
+		t.Error("dirContainsRegex(\"../misc/tests/dir\", \"^efgh\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
 	}
 }
