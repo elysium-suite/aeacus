a08d28962c263c5c05c109364abf4c51f7f5b5e3
Safin Singh <safin.singh@gmail.com>
2020-08-28 16:24:26 -0700

    RegistryKeyExists test
    Co-authored-by: Aadit Gupta <spacegiant21@gmail.com>

---
+++ cmd/checks_windows_test.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestRegistryKeyExists(t *testing.T) {
+	out, err := registryKey("HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CrashControl", "Overwrite", true)
+	if err != nil || out != true {
+		t.Error("registryKey(`HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\CrashControl`, \"Overwrite\") got " + boolToString(out) + ", want `true`. Error " + err.Error())
+	}
+}
