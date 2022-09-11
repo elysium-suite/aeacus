699f966d1123d977e7dc033c28f6e0b635b3ca42
sourque <contact@sourque.com>
2022-09-10 17:40:23 -0700

    Add AES-GCM to default crypto.go
    Credit to syossu#2202 for revealing an issue in the previous
    default implementation.

--- misc/dev/gen-crypto.sh
+++ misc/dev/gen-crypto.sh
@@ -4,9 +4,9 @@ set -e
 rand() { xxd -l 64 -c 64 -p /dev/urandom; }
 replace() { sed -i "s/$1/$2/g" crypto.go; }
 
-hashOne=$(rand)
-hashTwo=$(rand)
+hashVal=$(rand)
 byteKey=$(rand | sed 's/\(..\)/0x\1, /g')
+randomBytes=$(rand | sed 's/\(..\)/0x\1, /g')
 
 if [ -f "crypto.go.bak" ]; then
 	mv crypto.go.bak crypto.go
@@ -14,8 +14,8 @@ fi
 
 cp crypto.go crypto.go.bak
 
-replace "HASH_ONE" "$hashOne"
-replace "SECOND_HASH" "$hashTwo"
+replace "HASH_HERE" "$hashVal"
 replace "0x01" "`echo $byteKey | head -c 382`"
+replace "{1}" "{`echo $randomBytes | head -c 382`}"
 
 echo "Generated random keys for crypto.go"
